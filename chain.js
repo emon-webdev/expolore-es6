f4db6c7a69c68b92c1bd2e0f5d9dc2832faae0c1
emon-webdev <emon.webdev@gamil.com>
2024-07-15 12:18:52 +0600

    chaining

---
+++ chain.js
@@ -0,0 +1,16 @@
+// data access
+const data = [{id:1, name:'abul', adderss:'kochu khet'}]
+
+// console.log(data[0].adderss);
+
+
+const products={
+    count: 50909,
+    data:[
+        {id:1, name:"lenovo", price:6500},
+        {id:2, name:"macbook", price:16500},
+        {id:3, name:"iphone", price:5500},
+    ]
+}
+
+console.log(products.data[1].price);
---
+++ note.js
@@ -0,0 +1,11 @@
+/* 
+1. Var let const 
+2. default parameter
+3. template string 
+4. arrow function
+5. destructuring and spread operator
+6. Object.keys, values
+7. for of, for in
+8. 
+
+*/
